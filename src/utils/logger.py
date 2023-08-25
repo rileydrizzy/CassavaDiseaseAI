8886a8f8ca74e51f8cd06b1f5473a4074dc0a62a
Ladipo Ipadeola <rileydrizzy@hotmail.com>
2023-08-25 04:03:00 +0100

    update logger.py doc

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,20 +1,33 @@
-"""doc
+"""
+Module: Logging Utility
 
+This module provides a simple utility for logging messages to a file and the console.
+logging_utility
+
+Usage:
+- Import the module: `import logger`
+- Log messages with different levels:
+  - `logger.debug("Debug message")`
+  - `logger.info("Informational message")`
+  - `logger.warning("Warning message")`
+  - `logger.error("Error message")`
+  - `logger.critical("Critical message")`
 """
 
 from pathlib import Path
+
 from loguru import logger
 
-print("running okay")
+FORMAT_STYLE = (
+    "{time:MMMM D, YYYY > HH:mm:ss}  | {level} | {module}: {function}{line} - {message}"
+)
 
-FORMAT_STYLE = "{time:YYYY_MM_DD HH:mm:ss} | {level} | {module}: {function}{line} - {message}"
-
-LOG_DIR = 'logs'
+LOG_DIR = Path("logs")
 log_filepath = Path(LOG_DIR, "running_logs.log")
-Path.mkdir(LOG_DIR, exist_ok= True)
+Path.mkdir(LOG_DIR, exist_ok=True)
 
-#os.makedirs(log_dir, exist_ok= True)
-
-logger.add(log_filepath,
-    format= FORMAT_STYLE,
-    level= "INFO",)
+logger.add(
+    log_filepath,
+    format=FORMAT_STYLE,
+    level="INFO",
+)
